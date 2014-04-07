0d8f761c0552565d26d1852d78903ddf7127ac68
Sam Milechman <sammilechman@gmail.com>
2014-04-07 13:58:36 -0700

    Refined tile class, added neighbors

--- minesweeper.rb
+++ minesweeper.rb
@@ -29,8 +29,42 @@ def save
 def load
 =end
 
+
+class Board
+
+end
+
 class Tile
-  def initialize
+  attr_reader :position
+  def initialize(position)
+    @position = position
 
   end
+
+  def reveal
+
+  end
+
+  def neighbors
+    deltas = [[-1, 1], [0, 1], [1, 1], [-1, 0], [-1, -1], [1, -1], [1, 0], [0, -1]]
+    neighbors = deltas.map do |dx, dy|
+      [position[0] + dx, position[1] + dy]
+    end
+
+    neighbors.select { |value| on_board?(value) }
+  end
+
+  def on_board?(position)
+    return false if position[0] < 0 || position[0] > 8
+    return false if position[1] < 0 || position[1] > 8
+    true
+  end
+
+  def neighbor_bomb_count
+
+  end
+
 end
+
+a = Tile.new([0,0])
+p a.neighbors
