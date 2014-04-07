37b52f4588028ac7142899d8c09eaf91a270659e
Sam Milechman <sammilechman@gmail.com>
2014-04-07 13:27:41 -0700

    Outline project, create Tile class

---
+++ minesweeper.rb
@@ -0,0 +1,36 @@
+=begin
+10 Mines, 81 squares, distributed randomly.
+
+Reveal = Spacebar, Flag = f
+
+Classes: Tile, Board, User, Game
+
+Tile Class:
+def reveal
+def neighbors
+def neighbor_bomb_count
+
+
+Board Class:
+def print_board
+def create_new_board
+def seed_bombs
+def process_guess
+
+
+User Class:
+def guess
+def place_flag
+
+
+Game Class:
+def play
+def save
+def load
+=end
+
+class Tile
+  def initialize
+
+  end
+end
